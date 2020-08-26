9a5f5d09ca13a205a2061bf33608f85f5f9a4275
Nischay Sharma <sharmanischay23@gmail.com>
2020-08-26 14:25:47 +0530

    session added

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,50 @@
+
+function theme(option){
+  const night_button=document.querySelector('.controller_button');
+  const night_bar=document.querySelector('.controller_base');
+  const night_icon=document.querySelector('#night_mode');
+  
+  if(option==0)
+  {
+    night_button.style.transform="translateX(25px)";
+    night_button.style.transition="all 0.5s ease-out";
+    night_bar.style.background="orangered";
+    night_icon.style.color="yellow";
+    // INPUT FIELD
+    document.querySelector('input').style.background="black";
+  document.querySelector('input').style.color="white";
+  // BODY
+  document.body.style.background="black";
+  document.body.style.color="white";
+  // td
+  document.querySelector('td').style.background="black";
+  document.querySelector('td').style.color="white";
+  // PAGE
+  document.querySelector('.page').style.borderColor="white";
+
+    return 1;
+  }
+  else 
+  {
+    night_button.style.transform="translateX(0px)";
+    night_bar.style.background="grey";
+    night_icon.style.color="black";
+    // INPUT FIELD
+    document.querySelector('input').style.background="#fff";
+   document.querySelector('input').style.color="black";
+  //  BODY
+  document.body.style.background="none";
+  document.body.style.color="black";
+  // td
+   document.querySelector('td').style.background="white";
+   document.querySelector('td').style.color="black";
+  // PAGE
+  document.querySelector('.page').style.borderColor="grey";
+    
+  return 0;
+  }
+}
+
 var mode = 0;
 document.addEventListener("DOMContentLoaded", () => {
   var range = 0;
@@ -19,51 +66,45 @@ document.addEventListener("DOMContentLoaded", () => {
 //                                               DARK MODE
   //  ===================================================
   // ===================================================
-  var night_mode=false;
-  const night_bar=document.querySelector('.controller_base');
+
+  if(!localStorage.getItem('mode')){
+    localStorage.setItem('mode', 0);
+  }
+  else{
+    console.log(localStorage.getItem('mode'));
+  }
+
   const night_button=document.querySelector('.controller_button');
+  const night_bar=document.querySelector('.controller_base');
   const night_icon=document.querySelector('#night_mode');
+  var night_mode=localStorage.getItem('mode');
+  if (night_mode==1)
+  {
+    night_button.style.transform="translateX(25px)";
+    night_button.style.transition="all 0.5s ease-out";
+    night_bar.style.background="orangered";
+    night_icon.style.color="yellow";
+    // INPUT FIELD
+    document.querySelector('input').style.background="black";
+  document.querySelector('input').style.color="white";
+  // BODY
+  document.body.style.background="black";
+  document.body.style.color="white";
+  // td
+  document.querySelector('td').style.background="black";
+  document.querySelector('td').style.color="white";
+  // PAGE
+  document.querySelector('.page').style.borderColor="white";
+  }
+
+  
 
   // FUNCTION
   night_bar.onclick=()=>{
-    if(!night_mode)
-    {
-      night_button.style.transform="translateX(25px)";
-      night_button.style.transition="all 0.5s ease-out";
-      night_bar.style.background="orangered";
-      night_icon.style.color="yellow";
-      // INPUT FIELD
-      document.querySelector('input').style.background="black";
-    document.querySelector('input').style.color="white";
-    // BODY
-    document.body.style.background="black";
-    document.body.style.color="white";
-    // td
-    document.querySelector('td').style.background="black";
-    document.querySelector('td').style.color="white";
-    // PAGE
-    document.querySelector('.page').style.borderColor="white";
 
-      night_mode=true;
-    }
-    else
-    {
-      night_button.style.transform="translateX(0px)";
-      night_bar.style.background="grey";
-      night_icon.style.color="black";
-      // INPUT FIELD
-      document.querySelector('input').style.background="#fff";
-     document.querySelector('input').style.color="black";
-    //  BODY
-    document.body.style.background="none";
-    document.body.style.color="black";
-    // td
-     document.querySelector('td').style.background="white";
-     document.querySelector('td').style.color="black";
-    // PAGE
-    document.querySelector('.page').style.borderColor="grey";
-      night_mode=false;
-    }
+    night_mode=theme(night_mode);
+
+    localStorage.setItem('mode',night_mode);
   }
 // ===============================================
 // =================DARK MODE====================
@@ -98,8 +139,12 @@ document.addEventListener("DOMContentLoaded", () => {
           ) {
             alert("Version: 1.2 beta \nLanguage: ECMAScript6\nEngine : Spider Monkey \nCopyrights: Scorp Industies");
           }else if(passcode== "latest update" || passcode=="update"){
-            alert("1. Enhanced Mobile Compatibility.\n2.New Feature: Dark Mode \n3. New help and Support.");
+            alert("1. Enhanced Mobile Compatibility.\n2.New Feature: Dark Mode \n3. New help and Support.\n 4. Mode Changes are Dynamic");
           } 
+          else if(passcode=="dark mode")
+          {
+            alert("Dark mode is added to in update 1.2 on August 26 . It can remain there even after you have closed the Application.\n Copyrights: Scorp Industries");
+          }
           else if (passcode == "mikku") {
             alert("I love You");
           } 
